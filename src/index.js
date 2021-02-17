513c2762ade331017cbc2e45dd404c7f29e25815
José Ortega <jose.ortega.soto@gmail.com>
2021-02-17 12:39:50 -0300

    se implementa babel para el transpilado a ES5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const http = require('http')
-const app = express()
+import express from 'express'
+import http from 'http'
 
+const app = express()
 const servidor = http.createServer(app)
 
 servidor.listen(3001, () => console.log('Servidor iniciado'))
