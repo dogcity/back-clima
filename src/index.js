e081a7daf7c43053e28b8ceb6c71f6783b40015c
José Ortega <jose.ortega.soto@gmail.com>
2021-02-17 17:33:20 -0300

    se agrega socket.io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import express from 'express'
 import http from 'http'
+import socketio from 'socket.io'
 
 const app = express()
 const servidor = http.createServer(app)
+const io = socketio(servidor, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', (socket) => {
+  socket.on('conectado', () => {
+    console.log('usuario conectado', socket.id)
+  })
+
+  socket.on('registrar', (action) => {
+    console.log('Acción registrada', action)
+  })
+})
 
 servidor.listen(3001, () => console.log('Servidor iniciado'))
